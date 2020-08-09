e5a58f26d38107662ca0886ddd7410ce5cf2e0c1
nihonbuson <nihonbuson@gmail.com>
2020-08-10 02:54:13 +0900

    テストケースを階層構造化する

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,56 +1,64 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void _updateQualityを実行してもitem名が変わらない() {
-        Item[] items = new Item[] { new Item("foo", 10, 20) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+    @Nested
+    class _通常のItem{
+        @Test
+        void _updateQualityを実行してもitem名が変わらない() {
+            Item[] items = new Item[] { new Item("foo", 10, 20) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals("foo", app.items[0].name);
+        }
+
+        @Test
+        void _updateQualityを実行するとsellIn値が1減る() {
+            Item[] items = new Item[] { new Item("foo", 10, 20) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(9, app.items[0].sellIn);
+        }
+
+        @Test
+        void _updateQualityを実行するとquality値が1減る() {
+            Item[] items = new Item[] { new Item("foo", 10, 20) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(19, app.items[0].quality);
+        }
     }
 
-    @Test
-    void _updateQualityを実行するとsellIn値が1減る() {
-        Item[] items = new Item[] { new Item("foo", 10, 20) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(9, app.items[0].sellIn);
+    @Nested
+    class _Aged_BrieのItem{
+        @Test
+        void _Aged_BrieのitemでupdateQualityを実行してもitem名が変わらない() {
+            Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals("Aged Brie", app.items[0].name);
+        }
+
+        @Test
+        void _Aged_BrieのitemでupdateQualityを実行するとsellIn値が1減る() {
+            Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(9, app.items[0].sellIn);
+        }
+
+        @Test
+        void _Aged_BrieのitemでupdateQualityを実行するとquality値が1増える() {
+            Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(21, app.items[0].quality);
+        }
     }
 
-    @Test
-    void _updateQualityを実行するとquality値が1減る() {
-        Item[] items = new Item[] { new Item("foo", 10, 20) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(19, app.items[0].quality);
-    }
-
-    @Test
-    void _Aged_BrieのitemでupdateQualityを実行してもitem名が変わらない() {
-        Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("Aged Brie", app.items[0].name);
-    }
-
-    @Test
-    void _Aged_BrieのitemでupdateQualityを実行するとsellIn値が1減る() {
-        Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(9, app.items[0].sellIn);
-    }
-
-    @Test
-    void _Aged_BrieのitemでupdateQualityを実行するとquality値が1増える() {
-        Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(21, app.items[0].quality);
-    }
 }
