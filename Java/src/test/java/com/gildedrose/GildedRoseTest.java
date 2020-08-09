14d4294e3c6503a518065e6cffa77490dff60598
nihonbuson <nihonbuson@gmail.com>
2020-08-10 02:44:54 +0900

    テストケース名を変更

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void _updateQualityを実行してもitem名が変わらない() {
+        Item[] items = new Item[] { new Item("foo", 10, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
-
 }
