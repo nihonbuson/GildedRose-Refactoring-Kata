fd0a6d29425c7b16af370b0ddb1b0bf701a4cc88
nihonbuson <nihonbuson@gmail.com>
2020-08-11 18:27:28 +0900

    同様に他のItemの種類ごとにif文をまとめた。

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -46,24 +46,24 @@ class GildedRose {
                     }
                 }
             }
+            if (item.sellIn < 0) {
+                item.quality = 0;
+            }
         } else {
             if (item.quality > 0) {
                 decreaseQuality(item);
+            }
+            if (item.sellIn < 0) {
+                if (item.quality > 0) {
+                    decreaseQuality(item);
+                }
             }
         }
 
         if (isAgedBrie(item)) {
         } else {
             if (isBackstagePass(item)) {
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                }
             } else {
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        decreaseQuality(item);
-                    }
-                }
             }
         }
     }
