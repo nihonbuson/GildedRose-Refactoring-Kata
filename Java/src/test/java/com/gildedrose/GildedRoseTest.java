5810dc2104c6e890f7103981fb6cc627057b033f
nihonbuson <nihonbuson@gmail.com>
2020-08-11 17:56:16 +0900

    BackStageのテストケースを追加

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -62,6 +62,33 @@ class GildedRoseTest {
     }
 
     @Nested
+    class _BackstageのItem{
+        @Test
+        void _BackstageのitemでupdateQualityを実行してもitem名が変わらない() {
+            Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        }
+
+        @Test
+        void _BackstageのitemでupdateQualityを実行するとsellIn値が1減る() {
+            Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(14, app.items[0].sellIn);
+        }
+
+        @Test
+        void _BackstageのitemでupdateQualityを実行するとquality値が1増える() {
+            Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(21, app.items[0].quality);
+        }
+    }
+
+    @Nested
     class _SulfurasのItem{
         @Test
         void _SulfurasのitemでupdateQualityを実行してもitem名が変わらない() {
