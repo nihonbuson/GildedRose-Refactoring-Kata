097df550d70350bb6e7f07160b7df1942d829ed6
nihonbuson <nihonbuson@gmail.com>
2020-08-11 18:24:59 +0900

    インデントを揃えた

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -47,25 +47,25 @@ class GildedRose {
             }
         }
 
-            if (isAgedBrie(item)) {
+        if (isAgedBrie(item)) {
+            if (item.sellIn < 0) {
+                if (item.quality < 50) {
+                    increaseQuality(item);
+                }
+            }
+        } else {
+            if (isBackstagePass(item)) {
                 if (item.sellIn < 0) {
-                    if (item.quality < 50) {
-                        increaseQuality(item);
-                    }
+                    item.quality = 0;
                 }
             } else {
-                if (isBackstagePass(item)) {
-                    if (item.sellIn < 0) {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.sellIn < 0) {
-                        if (item.quality > 0) {
-                            decreaseQuality(item);
-                        }
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        decreaseQuality(item);
                     }
                 }
             }
+        }
     }
 
     private void updateSulfurasItem(Item item) {
