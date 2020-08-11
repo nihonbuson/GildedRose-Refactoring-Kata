40601be5a125835c8f1eb4b8de70ac47d106dd4c
nihonbuson <nihonbuson@gmail.com>
2020-08-11 18:24:21 +0900

    if (item.sellIn < 0)の条件文をそれぞれのItemのif文の中に入れた。

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -47,22 +47,25 @@ class GildedRose {
             }
         }
 
-        if (item.sellIn < 0) {
             if (isAgedBrie(item)) {
-                if (item.quality < 50) {
-                    increaseQuality(item);
+                if (item.sellIn < 0) {
+                    if (item.quality < 50) {
+                        increaseQuality(item);
+                    }
                 }
             } else {
                 if (isBackstagePass(item)) {
-                    item.quality = 0;
+                    if (item.sellIn < 0) {
+                        item.quality = 0;
+                    }
                 } else {
-                    if (item.quality > 0) {
-                        decreaseQuality(item);
+                    if (item.sellIn < 0) {
+                        if (item.quality > 0) {
+                            decreaseQuality(item);
+                        }
                     }
                 }
             }
-        }
-
     }
 
     private void updateSulfurasItem(Item item) {
