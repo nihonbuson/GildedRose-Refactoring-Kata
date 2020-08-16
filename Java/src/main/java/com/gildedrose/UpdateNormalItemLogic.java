4d7552e6f39656b0dee2edf8c941402ff6b41593
nihonbuson <nihonbuson@gmail.com>
2020-08-16 15:21:23 +0900

    通常商品についての実装クラスを作成

---
+++ Java/src/main/java/com/gildedrose/UpdateNormalItemLogic.java
@@ -0,0 +1,24 @@
+package com.gildedrose;
+
+public class UpdateNormalItemLogic implements UpdateItemLogic {
+    @Override
+    public void updateSellIn(Item item) {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    @Override
+    public void updateQuality(Item item) {
+        if (item.quality > 0) {
+            decreaseQuality(item);
+        }
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
+                decreaseQuality(item);
+            }
+        }
+    }
+
+    void decreaseQuality(Item item) {
+        item.quality = item.quality - 1;
+    }
+}
