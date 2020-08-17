30825ae14823508540a6e97af26d5936e11a8eaf
nihonbuson <nihonbuson@gmail.com>
2020-08-17 18:54:57 +0900

    不要なメソッドをすべて削除

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -15,7 +15,6 @@ class GildedRose {
 
     private void updateItem(Item item) {
         UpdateItemLogic updateItemLogic = selectItemLogic(item);
-
         updateItemLogic.updateSellIn(item);
         updateItemLogic.updateQuality(item);
     }
@@ -33,57 +32,6 @@ class GildedRose {
         return new UpdateNormalItemLogic();
     }
 
-    private void decreaseSellIn(Item item) {
-        item.sellIn = item.sellIn - 1;
-    }
-
-    private void updateNormalQuality(Item item) {
-        if (item.quality > 0) {
-            decreaseQuality(item);
-        }
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
-                decreaseQuality(item);
-            }
-        }
-    }
-
-    private void updateBackstageQuality(Item item) {
-        if (item.quality < 50) {
-            increaseQuality(item);
-
-            if (item.sellIn < 10) {
-                if (item.quality < 50) {
-                    increaseQuality(item);
-                }
-            }
-
-            if (item.sellIn < 5) {
-                if (item.quality < 50) {
-                    increaseQuality(item);
-                }
-            }
-        }
-        if (item.sellIn < 0) {
-            item.quality = 0;
-        }
-    }
-
-    private void updateAgedBrieQuality(Item item) {
-        if (item.quality < 50) {
-            increaseQuality(item);
-        }
-        if (item.sellIn < 0) {
-            if (item.quality < 50) {
-                increaseQuality(item);
-            }
-        }
-    }
-
-    private void updateSulfurasItem(Item item) {
-
-    }
-
     private boolean isSulfuras(Item item) {
         return item.name.equals("Sulfuras, Hand of Ragnaros");
     }
@@ -94,13 +42,5 @@ class GildedRose {
 
     private boolean isAgedBrie(Item item) {
         return item.name.equals("Aged Brie");
-    }
-
-    private void decreaseQuality(Item item) {
-        item.quality = item.quality - 1;
-    }
-
-    private void increaseQuality(Item item) {
-        item.quality = item.quality + 1;
     }
 }
