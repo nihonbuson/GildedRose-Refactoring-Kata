b72c5284589c270b1bdeac9be7b21c2f95ca265e
nihonbuson <nihonbuson@gmail.com>
2020-08-17 18:40:55 +0900

    同様にAgedBrieの場合のクラスも作成

---
+++ Java/src/main/java/com/gildedrose/UpdateAgedBrieItemLogicImpl.java
@@ -0,0 +1,24 @@
+package com.gildedrose;
+
+public class UpdateAgedBrieItemLogicImpl implements UpdateItemLogic {
+    @Override
+    public void updateSellIn(Item item) {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    @Override
+    public void updateQuality(Item item) {
+        if (item.quality < 50) {
+            increaseQuality(item);
+        }
+        if (item.sellIn < 0) {
+            if (item.quality < 50) {
+                increaseQuality(item);
+            }
+        }
+    }
+
+    private void increaseQuality(Item item) {
+        item.quality = item.quality + 1;
+    }
+}
