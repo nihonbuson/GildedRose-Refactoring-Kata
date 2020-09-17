a2bf86e84330be87d5a7b4e41456abd17390c665
nihonbuson <nihonbuson@gmail.com>
2020-09-17 18:00:00 +0900

    とりあえずGreenにした

--- Java/src/main/java/com/gildedrose/UpdateNormalItemLogic.java
+++ Java/src/main/java/com/gildedrose/UpdateNormalItemLogic.java
@@ -8,6 +8,11 @@ public class UpdateNormalItemLogic implements UpdateItemLogic {
 
     @Override
     public void updateQuality(Item item) {
+        if(item.name.equals("Conjured")){
+            item.decreaseQuality();
+            item.decreaseQuality();
+            return;
+        }
         if (item.isPlusValue()) {
             item.decreaseQuality();
         }
