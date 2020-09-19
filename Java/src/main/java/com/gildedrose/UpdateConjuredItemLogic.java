ac82c2dcd62e5a949f0fb7d5fbaaffc05c7adeb1
nihonbuson <nihonbuson@gmail.com>
2020-09-19 15:31:23 +0900

    UpdateConjuredItemLogicクラスに実装コードを記載して、ロジックをUpdateNormalItemLogicから分離させる。

--- Java/src/main/java/com/gildedrose/UpdateConjuredItemLogic.java
+++ Java/src/main/java/com/gildedrose/UpdateConjuredItemLogic.java
@@ -3,11 +3,12 @@ package com.gildedrose;
 public class UpdateConjuredItemLogic implements UpdateItemLogic {
     @Override
     public void updateSellIn(Item item) {
-
+        item.decreaseSellIn();
     }
 
     @Override
     public void updateQuality(Item item) {
-
+        item.decreaseQuality();
+        item.decreaseQuality();
     }
 }
--- Java/src/main/java/com/gildedrose/UpdateNormalItemLogic.java
+++ Java/src/main/java/com/gildedrose/UpdateNormalItemLogic.java
@@ -8,11 +8,6 @@ public class UpdateNormalItemLogic implements UpdateItemLogic {
 
     @Override
     public void updateQuality(Item item) {
-        if(item.name.equals("Conjured")){
-            item.decreaseQuality();
-            item.decreaseQuality();
-            return;
-        }
         if (item.isPlusValue()) {
             item.decreaseQuality();
         }
