59ba78c8d9946fc56dff19d81245d5ba3b8912b9
nihonbuson <nihonbuson@gmail.com>
2020-11-15 20:23:35 +0900

    インデントを揃える

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -21,7 +21,7 @@ class GildedRose {
         if (!isAgedBrie(item)
                 && !isBackstagePass(item)) {
             if (item.quality > 0) {
-                    item.quality = item.quality - 1;
+                item.quality = item.quality - 1;
             }
         } else {
             if (item.quality < 50) {
@@ -43,13 +43,13 @@ class GildedRose {
             }
         }
 
-            item.sellIn = item.sellIn - 1;
+        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
             if (!isAgedBrie(item)) {
                 if (!isBackstagePass(item)) {
                     if (item.quality > 0) {
-                            item.quality = item.quality - 1;
+                        item.quality = item.quality - 1;
                     }
                 } else {
                     item.quality = item.quality - item.quality;
