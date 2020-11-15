86cc13a638b716737f30a854a0f8bee2541afe1d
nihonbuson <nihonbuson@gmail.com>
2020-11-16 01:08:09 +0900

    Quality値の増加をメソッド化

--- Java/src/main/java/com/gildedrose/AgedBrieItem.java
+++ Java/src/main/java/com/gildedrose/AgedBrieItem.java
@@ -10,14 +10,16 @@ public class AgedBrieItem implements ICategorizedItem {
 
 	@Override
 	public void updateItem() {
-		if (item.quality < 50) {
-			item.quality = item.quality + 1;
-		}
+		increaseQuality();
 		item.sellIn = item.sellIn - 1;
 		if (item.sellIn < 0) {
-			if (item.quality < 50) {
-				item.quality = item.quality + 1;
-			}
+			increaseQuality();
+		}
+	}
+
+	private void increaseQuality() {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
 		}
 	}
 }
