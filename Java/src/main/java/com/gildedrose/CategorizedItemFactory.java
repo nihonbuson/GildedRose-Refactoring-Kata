a043fe63456bbd845befe7b7e7016607d444b4f1
nihonbuson <nihonbuson@gmail.com>
2020-11-15 23:27:43 +0900

    文字列を変数化

--- Java/src/main/java/com/gildedrose/CategorizedItemFactory.java
+++ Java/src/main/java/com/gildedrose/CategorizedItemFactory.java
@@ -1,6 +1,11 @@
 package com.gildedrose;
 
 public class CategorizedItemFactory {
+
+	private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+	private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+	private static final String AGED_BRIE = "Aged Brie";
+
 	public static ICategorizedItem create(Item item) {
 		if (isSulfuras(item)) {
 			return new SulfurasItem(item);
@@ -13,15 +18,15 @@ public class CategorizedItemFactory {
 		}
 	}
 	private static boolean isSulfuras(Item item) {
-		return item.name.equals("Sulfuras, Hand of Ragnaros");
+		return item.name.equals(SULFURAS);
 	}
 
 	private static boolean isBackstagePass(Item item) {
-		return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+		return item.name.equals(BACKSTAGE_PASSES);
 	}
 
 	private static boolean isAgedBrie(Item item) {
-		return item.name.equals("Aged Brie");
+		return item.name.equals(AGED_BRIE);
 	}
 
 }
