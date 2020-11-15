48227d21d68c15afe56b3120f7a9a970f9ce8ffd
nihonbuson <nihonbuson@gmail.com>
2020-11-15 19:53:06 +0900

    通常商品のSellIn値とQuality値のテストを追加

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void _通常商品で更新しても商品名は変わらない() {
+        Item[] items = new Item[] { new Item("foo", 20, 30) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    void _通常商品で更新するとsellIn値が1減る() {
+        Item[] items = new Item[] { new Item("foo", 20, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(19, app.items[0].sellIn);
+    }
+
+    @Test
+    void _通常商品で更新するとquality値が1減る() {
+        Item[] items = new Item[] { new Item("foo", 20, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(29, app.items[0].quality);
     }
 
 }
