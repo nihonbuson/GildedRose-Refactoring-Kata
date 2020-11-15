87be287494e16bab74d6d8cf27dd178a3e71bf82
nihonbuson <nihonbuson@gmail.com>
2020-11-15 20:21:51 +0900

    !isSulfuras(item)の判定式を削除

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -21,9 +21,7 @@ class GildedRose {
         if (!isAgedBrie(item)
                 && !isBackstagePass(item)) {
             if (item.quality > 0) {
-                if (!isSulfuras(item)) {
                     item.quality = item.quality - 1;
-                }
             }
         } else {
             if (item.quality < 50) {
@@ -45,17 +43,13 @@ class GildedRose {
             }
         }
 
-        if (!isSulfuras(item)) {
             item.sellIn = item.sellIn - 1;
-        }
 
         if (item.sellIn < 0) {
             if (!isAgedBrie(item)) {
                 if (!isBackstagePass(item)) {
                     if (item.quality > 0) {
-                        if (!isSulfuras(item)) {
                             item.quality = item.quality - 1;
-                        }
                     }
                 } else {
                     item.quality = item.quality - item.quality;
