4557dcb65fe937867dc6e416c89ee96187fd7583
nihonbuson <nihonbuson@gmail.com>
2020-11-15 22:47:59 +0900

    それぞれの商品ごとのif文にまとめる

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -23,6 +23,11 @@ class GildedRose {
                 item.quality = item.quality + 1;
             }
             item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 0) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
         } else if (isBackstagePass(item)) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
@@ -40,31 +45,26 @@ class GildedRose {
                 }
             }
             item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 0) {
+                item.quality = item.quality - item.quality;
+            }
         } else {
             if (item.quality > 0) {
                 item.quality = item.quality - 1;
             }
             item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 0) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+            }
         }
 
 
         if (isAgedBrie(item)) {
-            if (item.sellIn < 0) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
         } else {
             if (isBackstagePass(item)) {
-                if (item.sellIn < 0) {
-                    item.quality = item.quality - item.quality;
-                }
             } else {
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                }
             }
         }
     }
