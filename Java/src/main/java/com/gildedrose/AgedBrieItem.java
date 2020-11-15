2db1b8ac2bc19783b89bf733fa648acb1073d701
nihonbuson <nihonbuson@gmail.com>
2020-11-15 23:03:26 +0900

    AgedBrieItemクラスを追加

---
+++ Java/src/main/java/com/gildedrose/AgedBrieItem.java
@@ -0,0 +1,15 @@
+package com.gildedrose;
+
+public class AgedBrieItem implements ICategorizedItem {
+	private Item item;
+
+	public AgedBrieItem(Item item) {
+
+		this.item = item;
+	}
+
+	@Override
+	public void updateItem() {
+
+	}
+}
