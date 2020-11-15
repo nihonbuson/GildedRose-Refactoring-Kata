bf6acebf78ed22c12b1e23cd1a5160dc9485c7df
nihonbuson <nihonbuson@gmail.com>
2020-11-15 20:01:43 +0900

    通常商品のパターンをネストする

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,37 +1,42 @@
 package com.gildedrose;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
-
     Item[] items;
-    @BeforeEach
-    void setup(){
-        items = new Item[] { new Item("foo", 20, 30) };
-    }
 
-    @Test
-    void _通常商品で更新しても商品名は変わらない() {
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-    }
+    @Nested
+    class _通常商品の場合 {
+        @BeforeEach
+        void setup(){
+            items = new Item[] { new Item("foo", 20, 30) };
+        }
 
-    @Test
-    void _通常商品で更新するとsellIn値が1減る() {
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(19, app.items[0].sellIn);
-    }
+        @Test
+        void _通常商品で更新しても商品名は変わらない() {
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals("foo", app.items[0].name);
+        }
 
-    @Test
-    void _通常商品で更新するとquality値が1減る() {
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(29, app.items[0].quality);
+        @Test
+        void _通常商品で更新するとsellIn値が1減る() {
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(19, app.items[0].sellIn);
+        }
+
+        @Test
+        void _通常商品で更新するとquality値が1減る() {
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(29, app.items[0].quality);
+        }
+
     }
 
     @Test
