d8c2bd20d4da820685970e7543bf2728c6624662
nihonbuson <nihonbuson@gmail.com>
2020-11-15 19:58:48 +0900

    Sulfurasのテストケースを追加

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -34,4 +34,30 @@ class GildedRoseTest {
         assertEquals(29, app.items[0].quality);
     }
 
+    @Test
+    void _Sulfurasで更新しても商品名は変わらない() {
+        items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 20, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+    }
+
+    @Test
+    void _Sulfurasで更新してもsellIn値は変わらない() {
+        items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 20, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(20, app.items[0].sellIn);
+    }
+
+    @Test
+    void _Sulfurasで更新してもquality値は変わらない() {
+        items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 20, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(30, app.items[0].quality);
+    }
+
+
+
 }
